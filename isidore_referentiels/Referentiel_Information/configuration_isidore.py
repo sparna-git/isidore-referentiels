a4044fd28062027c0e2867e4b924f41f68b79d3b
AQUINO SANCHEZ Jorge <jorge.aquinosanchez@ext.europarl.europa.eu>
2024-10-14 12:11:21 +0200

    update referentiel information

--- isidore_referentiels/Referentiel_Information/configuration_isidore.py
+++ isidore_referentiels/Referentiel_Information/configuration_isidore.py
@@ -10,34 +10,40 @@ class readConfiguration:
 
     def __init__(self, Data_yaml_Global):
         self.conf = yaml.safe_load(Data_yaml_Global)
-        self.root_directory = Path(''.join(self.conf["data"])).parent.cwd()
-        print(self.root_directory)
+        
         self.Referentiel = self.conf["id"]
-        self.logger = self.create_logging()
+        # Create directory Root
+        self.WorkDir = self.conf["WorkDir"]
+        self.WorkDirectory = self.__createDirectory(self.conf["WorkDir"])
+        self.OutputDir = self.conf["OutputDir"]
+        self.OutputDirectory = self.__createDirectory(self.conf["OutputDir"])
+        # Create répertoire pour le referentiel
+        self.path_referentiel = os.path.join(self.WorkDir,self.Referentiel)
+        self.Referentiel_directory = self.__createDirectory(self.path_referentiel)
+        # Répertoire Tmp
+        self.path_referentiel_tmp = os.path.join(self.path_referentiel,"tmp")
+        self.Referentiel_tmp_directory = self.__createDirectory(self.path_referentiel_tmp)
 
-    def create_logging(self):
+        self.loggingConfig = self.conf["logFile"]
+        self.logger = self.__create_logging()
+        #
+        self.sparql = self.conf["clean"]["sparql"]
+        self.report = self.conf["report"]
 
-        # Create directory
-        Log_Directory = os.path.join(self.root_directory,"logs")
-        if not os.path.exists(Log_Directory):
-            os.makedirs(Log_Directory)
-        else:
-            shutil.rmtree(Log_Directory)
+    def __create_logging(self):
 
         logger = logging
-        File_log = os.path.join(Log_Directory,f"{self.Referentiel}.txt")
-        logger.basicConfig(filename=File_log,level=logging.DEBUG,format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+        File_log = os.path.join(Path(self.path_referentiel).absolute(),self.loggingConfig)
+        logger.basicConfig(filename=File_log,level=logging.DEBUG,format="{asctime} - {levelname} - {message}",style="{" ,datefmt="%d-%m-%Y %H:%M")
+        
         return logger
 
-    def createDirectory(self,directory) -> str:
+    def __createDirectory(self,directory) -> str:
 
         pathDirectory = "./"+directory
         try:            
-            if os.path.exists(pathDirectory):
-                shutil.rmtree(pathDirectory)
-
-            newDirectory = os.mkdir(pathDirectory)
-            if newDirectory:
+            if not os.path.exists(pathDirectory):
+                newDirectory = os.mkdir(pathDirectory)
                 return f"Directory {pathDirectory} created successfully"
         except OSError:
             return f"Error: Failed to create directory {directory} - {OSError}"
@@ -56,11 +62,19 @@ class confReferentiel(readConfiguration):
         return ''.join(self.conf["data"])
 
     def get_Sparql(self) -> list:
-        return self.conf["sparql"]
+        return self.sparql
     
-    def get_Analize(self) -> str:
-        return ''.join(self.conf["Analyze"])
+    def get_Workdirectory(self) -> str:
+        return self.WorkDir
     
-    def get_Wikidata(self) -> str:
-        return ''.join(self.conf["Wikidata"])
-         
+    def get_Outputdirectory(self) -> str:
+        return self.OutputDir
+
+    def get_referentiel_directory(self) -> str:
+        return self.path_referentiel
+
+    def get_TmpDirectory(self) -> str:
+        return self.path_referentiel_tmp
+    
+    def get_Report(self) -> list:
+        return self.conf["report"]
