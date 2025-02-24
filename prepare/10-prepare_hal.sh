d023493afb1f1080f5bea0584169525a2f15ad5e
RefIsidore <refisidore@huma-num.fr>
2025-02-24 12:45:47 +0100

    Fix prepare HAL

--- prepare/10-prepare_hal.sh
+++ prepare/10-prepare_hal.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Referentiel HAL
-export OUTPUT=./data/hal
+export OUTPUT=../data/hal
 
 rm -rf $OUTPUT
 mkdir -p $OUTPUT
-poetry run hal --output ./data/hal
+poetry run hal --output $OUTPUT
 
 echo "Le fichier a été stocké dans le répertoire: " $OUTPUT
