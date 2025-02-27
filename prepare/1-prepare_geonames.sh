38b4852d51675644148769696c9f599a3d0affb3
RefIsidore <refisidore@huma-num.fr>
2025-02-27 12:56:28 +0100

    update geonames prepare script with 12 seconds

--- prepare/1-prepare_geonames.sh
+++ prepare/1-prepare_geonames.sh
@@ -7,6 +7,10 @@ rm -rf $OUTPUT
 mkdir -p $OUTPUT
 
 rm -rf geonames.log
-poetry run geonames --user humanumRef --output $OUTPUT
+poetry run geonames --user humanumRef --seconds 12 --output $OUTPUT
+
+# Si besoin, on peut donner un nombre maximum de notices a telecharger, pour tester sur un petit nombre
+# Avec le paramètre --maxrdf
+# poetry run geonames --user humanumRef --output $OUTPUT --maxrdf 10
 
 echo "Les fichiers ont été stockés dans le répertoire: " $OUTPUT
